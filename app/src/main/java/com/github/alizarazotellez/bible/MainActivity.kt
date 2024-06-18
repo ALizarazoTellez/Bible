e0c3499f867d6ba24751926016c3e1264c141fb3
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-06-18 17:26:38 -0500

    Add basic view

--- app/src/main/java/com/github/alizarazotellez/bible/MainActivity.kt
+++ app/src/main/java/com/github/alizarazotellez/bible/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.github.alizarazotellez.bible.ui.theme.BibleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +32,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BibleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainComponent()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainComponent(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = "Bible") }) },
+        bottomBar = { NavigationBar {
+            NavigationBarItem(selected = true, onClick = { /*TODO*/ }, icon = { Icon(
+                Icons.Default.Home,
+                contentDescription = null,
+            ) })
+            NavigationBarItem(selected = false, onClick = { /*TODO*/ }, icon = { Icon(
+                Icons.Default.Info,
+                contentDescription = null,
+            )})
+        }},
+        modifier = modifier) { padding ->
+        Column(modifier = Modifier
+            .padding(padding)
+            .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            Text(text = "Bible", fontSize = 30.sp)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun MainComponentPreview() {
     BibleTheme {
-        Greeting("Android")
+        MainComponent()
     }
 }
