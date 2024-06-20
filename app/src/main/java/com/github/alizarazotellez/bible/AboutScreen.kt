3bfd647ff25c8f1c21c48414ec575ee802274fb9
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-06-20 15:29:18 -0500

    Improve about screen

--- app/src/main/java/com/github/alizarazotellez/bible/AboutScreen.kt
+++ app/src/main/java/com/github/alizarazotellez/bible/AboutScreen.kt
@@ -1,24 +1,91 @@
 package com.github.alizarazotellez.bible
 
+import android.graphics.drawable.AdaptiveIconDrawable
+import android.os.Build
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toBitmap
 
 @Composable
 fun AboutScreen(padding: PaddingValues) {
     Column(
         modifier = Modifier
             .padding(padding)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .padding(16.dp)
     ) {
-        Text(text = "Made by Anderson.")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Image(
+                painter = adaptiveIconPainterResource(id = R.mipmap.ic_launcher_round),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Bible", fontSize = 30.sp)
+        }
+
+        Spacer(modifier = Modifier.height(64.dp))
+
+        Text(buildAnnotatedString {
+            append("Made by ")
+            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                append("Anderson")
+            }
+            append(".")
+        }, modifier = Modifier.align(alignment = Alignment.CenterHorizontally))
     }
+}
+
+@Composable
+fun adaptiveIconPainterResource(@DrawableRes id: Int): Painter {
+    val res = LocalContext.current.resources
+    val theme = LocalContext.current.theme
+
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        // Android O supports adaptive icons, try loading this first (even though this is least likely to be the format).
+        val adaptiveIcon = ResourcesCompat.getDrawable(res, id, theme) as? AdaptiveIconDrawable
+        if (adaptiveIcon != null) {
+            BitmapPainter(adaptiveIcon.toBitmap().asImageBitmap())
+        } else {
+            // We couldn't load the drawable as an Adaptive Icon, just use painterResource
+            painterResource(id)
+        }
+    } else {
+        // We're not on Android O or later, just use painterResource
+        painterResource(id)
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun AboutScreenPreview() {
+    AboutScreen(padding = PaddingValues(0.dp))
 }
