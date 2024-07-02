8c60f4747ea3471bd6241cc442ab1bb9226f24d9
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-07-02 14:58:00 -0500

    Add the AndroidX Lifecycle ViewModel Compose lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.navigation.dynamic.features.fragment)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
