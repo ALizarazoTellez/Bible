5bcb04e541f53661e4ebffa30ee2b370621d2790
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-07-02 14:38:10 -0500

    Improve performance

--- app/src/main/java/com/github/alizarazotellez/bible/Bible.kt
+++ app/src/main/java/com/github/alizarazotellez/bible/Bible.kt
@@ -1,25 +1,31 @@
 package com.github.alizarazotellez.bible
 
 import android.content.Context
+import androidx.compose.runtime.Immutable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
 
 @Serializable
+@Immutable
 data class Book(val Name: String, val Content: List<List<String>>)
 
 object Bible {
     private var books: List<Book> = listOf()
 
     @OptIn(ExperimentalSerializationApi::class)
-    fun load(context: Context) {
+    suspend fun load(context: Context) {
         if (books.isNotEmpty()) {
             return
         }
 
-        val stream = context.resources.openRawResource(R.raw.bible)
-        books = Json.decodeFromStream(stream)
+        withContext(Dispatchers.IO) {
+            val stream = context.resources.openRawResource(R.raw.bible)
+            books = Json.decodeFromStream(stream)
+        }
     }
 
     fun getBooks(): List<Book> {
