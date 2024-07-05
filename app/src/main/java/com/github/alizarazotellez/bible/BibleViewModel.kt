98a6289f4116797e8e60a107e13c1c2b9f3416bf
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-07-04 21:53:17 -0500

    Preserve state of the app when killed

--- app/src/main/java/com/github/alizarazotellez/bible/BibleViewModel.kt
+++ app/src/main/java/com/github/alizarazotellez/bible/BibleViewModel.kt
@@ -3,19 +3,22 @@ package com.github.alizarazotellez.bible
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class BibleViewModel : ViewModel() {
-    var currentBook by mutableIntStateOf(0)
+class BibleViewModel(private val state: SavedStateHandle) : ViewModel() {
+    var currentBook by mutableIntStateOf(state.get<Int?>("currentBook") ?: 0)
         private set
-    var currentChapter by mutableIntStateOf(0)
+    var currentChapter by mutableIntStateOf(state.get<Int?>("currentChapter") ?: 0)
         private set
 
     fun changeCurrentBook(book: Int) {
+        state["currentBook"] = book
         currentBook = book
     }
 
     fun changeCurrentChapter(chapter: Int) {
+        state["currentChapter"] = chapter
         currentChapter = chapter
     }
 }
